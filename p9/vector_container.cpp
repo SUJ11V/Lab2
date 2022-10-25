f3ba054b9d8ca6df45de355053e2262abd1b5a8b
SUJ11V <ejqw12@naver.com>
2022-10-25 10:19:37 +0900

    실습 문제 9번

--- p9/vector_container.cpp
+++ p9/vector_container.cpp
@@ -7,7 +7,8 @@ int main(){
 
 	int x;
 	int y;
-
+	int number;
+	int result;
 	cout << "열의 수를 입력해주세요: ";
 	cin >> x;
 
@@ -19,28 +20,27 @@ int main(){
 
 	cout << "첫 번째 행렬의 값을 입력해주세요\n";
 	for(int i=0; i<y; i++){
-		for(int j=0; j<x; j++){
-			cout << "a1[" << i << "][" << j << "] : ";
-			int n;
-			cin >> n;
-			a1[i].push_back(n);
+		for(int k=0; k<x; k++){
+			cout << "첫 번째 행렬[" << i << "][" << k << "] : ";
+			cin >> number;
+			a1[i].push_back(number);
 		}
 	}
 
 	cout << "두 번째 행렬의 값을 입력해주세요\n";
 	for(int i=0; i<y; i++){
-		for(int j=0; j<x; j++){
-			cout << "a2[" << i << "][" << j <<"] : ";
-			int n;
-			cin >> n;
-			a2[i].push_back(n);
+		for(int k=0; k<x; k++){
+			cout << "두 번째 행렬["<< i <<"]["<< k <<"] : ";
+			cin >> number;
+			a2[i].push_back(number);
 		}
 	}
 
-	cout << "첫 번째 행렬 + 두 번째 행렬\n";
+	cout << "결과 행렬\n";
 	for(int i=0; i<y; i++){
-		for(int j=0; j<x; j++){
-			cout << a1[i][j] + a2[i][j] << " "; 			
+		for(int k=0; k<x; k++){
+			result = a1[i][k]+a2[i][k]; 
+			cout << result <<" "; 			
 		}
 		cout << endl;
 	}
