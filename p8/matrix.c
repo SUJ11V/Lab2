b50cf14ffbeac5ccae4b2abb67799c0c7e2a6a1e
SUJ11V <ejqw12@naver.com>
2022-10-25 10:16:27 +0900

    실습 문제 8번

--- p8/matrix.c
+++ p8/matrix.c
@@ -5,39 +5,41 @@ int main()
 {
 	int x, y;
 	int **a1, **a2;
+	int result;
 	
 	printf("숫자 두 개를 입력하세요: ");
 	scanf("%d %d", &x, &y);
 
-	a1 = (int **)malloc(sizeof(int *) * x);
-	a2 = (int **)malloc(sizeof(int *) * x);
+	a1 = (int **)malloc(sizeof(int *) *x);
+	a2 = (int **)malloc(sizeof(int *) *x);
 
 	for(int i=0; i < x; i++){
-		a1[i] = (int *)malloc(sizeof(int *) * y);
-		a2[i] = (int *)malloc(sizeof(int *) * y);
+		a1[i] = (int *)malloc(sizeof(int *) *y);
+		a2[i] = (int *)malloc(sizeof(int *) *y);
 	}
 
 	printf("첫 행렬에 넣을 숫자를 입력하세요\n");
 	for(int i=0; i<x; i++){
-		for(int j=0; j<y; j++){
-			printf("a1[%d][%d] : ", i,j);
-			scanf("%d",&a1[i][j]);
+		for(int k=0; k<y; k++){
+			printf("첫 번째 행렬[%d][%d]: ", i,k);
+			scanf("%d",&a1[i][k]);
 		}
 	}
 	
 	printf("두 번째 행렬에 넣을 숫자를 입력하세요\n");
 	for(int i=0; i<x; i++){
-		for(int j=0; j<y; j++){
-			printf("a2[%d][%d] : ", i,j);
-			scanf("%d",&a2[i][j]);
+		for(int k=0; k<y; k++){
+			printf("두 번째 행렬[%d][%d]: ", i,k);
+			scanf("%d",&a2[i][k]);
 		}
 	}
 	
-	printf("첫 번째 행렬 + 두 번째 행렬\n");
+	printf("결과 행렬\n");
 
 	for(int i=0; i<x; i++){
-		for(int j=0; j<y; j++){
-			printf("%d ", a1[i][j] + a2[i][j]);	
+		for(int k=0; k<y; k++){
+			result=a1[i][k] + a2[i][k];
+			printf("%d ",result);	
 		}
 		printf("\n");
 	}
@@ -46,6 +48,7 @@ int main()
 		free(a1[i]);
 		free(a2[i]);
 	}
+	
 	free(a1);
 	free(a2);
 
